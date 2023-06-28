753c8fe4b8079dfa8588403fdd565e24b5677818
Jonas Imping <j.imping@splintnet.de>
2023-06-28 10:25:13 +0200

    impr.

---
+++ public/typo3conf/image_autoresize.config.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'directories' => '1:/',
+    'file_types' => 'jpg,jpeg,png',
+    'threshold' => '400K',
+    'max_width' => '5000',
+    'max_height' => '7000',
+    'max_size' => '100M',
+    'auto_orient' => '1',
+    'conversion_mapping' => 'ai => jpg,bmp => jpg,pcx => jpg,tga => jpg,tif => jpg,tiff => jpg',
+    'keep_metadata' => '0',
+    'resize_png_with_alpha' => '0',
+];
